1a3b50a2acdff8f11941a0070ede20ee4edc81d2
Juan Zapata <juan.zapata.199@gmail.com>
2024-05-28 04:01:24 -0500

    POR FIN EJECUTA MINECRAFT AAAAAAAAAAAA

--- test/testPack/McArgsCommand.java
+++ test/testPack/McArgsCommand.java
@@ -5,11 +5,12 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class McArgsCommand {
-    String mineDir, miyeisonString, yeisonFile;
-    JSONObject yeison, argsssss;
+    String mineDir, miyeisonString, yeisonFile, miyeisonStringVanilla, yeisonFileVanilla;
+    JSONObject yeison, argsssss, yeisonVanilla;
 
     public McArgsCommand(String mineDir) throws IOException {
         this.mineDir = mineDir;
@@ -17,6 +18,12 @@ public class McArgsCommand {
         miyeisonString = mineDir + "/versions/1.18.2-forge-40.2.21/1.18.2-forge-40.2.21.json";
         yeisonFile = new String(Files.readAllBytes(Paths.get(miyeisonString)));
         this.yeison = new JSONObject(yeisonFile);
+        
+        miyeisonStringVanilla = mineDir + "/versions/1.18.2/1.18.2.json";
+        yeisonFileVanilla = new String(Files.readAllBytes(Paths.get(miyeisonStringVanilla)));
+        this.yeisonVanilla = new JSONObject(yeisonFileVanilla);
+        
+        
         this.argsssss = yeison.getJSONObject("arguments");
     }
     
@@ -25,7 +32,35 @@ public class McArgsCommand {
         List<String> clp = new ArrayList<>();
         for (int i = 0; i < yeison.getJSONArray("libraries").length(); i++){
             String libsJAJA = yeison.getJSONArray("libraries").getJSONObject(i).getJSONObject("downloads").getJSONObject("artifact").getString("path");
-            clp.add(miyeisonString + "/" +libsJAJA);
+            clp.add(mineDir + "/libraries/" +libsJAJA);
+        }
+        result = String.join(";", clp);
+        return result;
+    }
+    
+    public String getCpLibsVanilla(){
+        String result;
+        List<String> clp = new ArrayList<>();
+        for (int i = 0; i < yeisonVanilla.getJSONArray("libraries").length(); i++){
+            String libsJAJA;
+            String pipi;
+            
+            try {
+                if (yeisonVanilla.getJSONArray("libraries").getJSONObject(i).getJSONArray("rules").length() == 1) {
+                continue;
+            }
+            } catch (Exception e) {
+            }
+            
+            try {
+                libsJAJA = yeisonVanilla.getJSONArray("libraries").getJSONObject(i).getJSONObject("downloads").getJSONObject("classifiers").getJSONObject("natives-windows").getString("path");
+                System.out.println("pene");
+            } catch (Exception e) {
+                libsJAJA = yeisonVanilla.getJSONArray("libraries").getJSONObject(i).getJSONObject("downloads").getJSONObject("artifact").getString("path");
+                System.out.println("vagina");
+            }
+            
+            clp.add(mineDir + "/libraries/" +libsJAJA);
         }
         result = String.join(";", clp);
         return result;
--- test/testPack/ejecutqador.java
+++ test/testPack/ejecutqador.java
@@ -4,10 +4,13 @@ import testPack.*;
 
 public class ejecutqador {
     public static void main(String[] args) throws IOException {
-        McArgsCommand ola = new McArgsCommand("C:/Users/"+ System.getProperty("user.name") +"/AppData/Roaming/.jnt-copia");
-        System.out.println(ola.getMainClassMC());
+        McArgsCommand mcArgsCommand = new McArgsCommand("C:/Users/"+ System.getProperty("user.name") +"/AppData/Roaming/.jnt-copia");
+        System.out.println(mcArgsCommand.getCpLibs() + ";" + mcArgsCommand.getCpLibsVanilla());
         
-        replacePalceholder pene = new replacePalceholder("C:/Users/"+ System.getProperty("user.name") +"/AppData/Roaming/.jnt-copia", "1.18.2");
-        System.out.println(pene.getPlacedHoldediiiii(ola.getJvmArgs()));
+        //System.out.println(ola.getMainClassMC());
+        //System.out.println(ola.getJvmArgs());
+        
+        //replacePalceholder pene = new replacePalceholder("C:/Users/"+ System.getProperty("user.name") +"/AppData/Roaming/.jnt-copia", "1.18.2");
+        //System.out.println(ola.getJvmArgs());
     }
 }
